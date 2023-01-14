b830e05b9f54cf8a2e9abae69b446fecc03c9dbe
OfficialCRUGG <ich@crugg.de>
2023-01-14 14:43:44 +0100

    ✨ Add ordinals

--- src/numbers.ts
+++ src/numbers.ts
@@ -5,3 +5,9 @@ export function prettyNumber(number: number, useCommas = true): string {
         return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
     }
 }
+
+export function ordinal(number: number, ordinals: [string, string, string, string] = ["st", "nd", "rd", "th"]): string {
+    if (number < 0) number *= -1;
+    if (number % 1 !== 0) number = Math.floor(number);
+    return ordinals.slice(0, 3)[((((number + 90) % 100) - 10) % 10) - 1] || ordinals[3];
+}
